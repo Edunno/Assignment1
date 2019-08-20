cae31e22a130f591ca1f13c4aaa893080f3bd53b
Edunno <tamericandream@hotmail.com>
2019-08-20 11:29:41 +0200

    Upload

---
+++ Assignment1/Assets/FirsCube.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirsCube : MonoBehaviour
+{
+    public Camera pCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    //Prøvede at få retningerne til at følge kameraets retning, men det lykkedes ikke :(
+    {
+        if(Input.GetKey(KeyCode.A)){
+            float camF = RunCam();
+            this.GetComponent<Rigidbody>().AddForce(-20-(40*camF),0,camF*20);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            float camF = RunCam();
+            this.GetComponent<Rigidbody>().AddForce(20+(40*camF),0,camF*20);
+        }
+        if(Input.GetKey(KeyCode.W)){
+            float camF = RunCam();
+            this.GetComponent<Rigidbody>().AddForce(camF*20,0,20*camF);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            float camF = RunCam();
+            this.GetComponent<Rigidbody>().AddForce(camF*-20,0,-20*camF);
+        }
+    }
+    float RunCam()
+    {
+        float camB;
+        float camY = pCam.transform.rotation.y;
+        if(camY>360||camY<-360){
+            camY = (camY%360);
+        }
+        print("first:"+camY);
+        if(camY>0){
+            camB = 1-camY;
+        }
+        else{
+            camB = camY;
+        }
+        print(camB);
+        return camB;
+    }
+}
---
+++ Assignment1/Assets/canvasscr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class canvasscr : MonoBehaviour
+{
+    public Button b;
+    public Text t;
+    public Button Sb;
+    public AudioSource s;
+    public Button Ib;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        t.text = "Click this";
+        b.onClick.AddListener(WhenClicked);
+        Sb.onClick.AddListener(PlaySound);
+        Ib.onClick.AddListener(MakeObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)){
+            print("Respect");
+        }
+        
+    }
+
+    void WhenClicked()
+    {
+        t.text = "Click noted";
+        print(t.text);
+        //GamObject.Find("whatevs").GetComponent<Rigidbody>().AddForce(0,20,0);
+    }
+
+    void PlaySound()
+    {
+        s.Play();
+    }
+
+    void MakeObject()
+    {
+        Vector3 myVector = new Vector3(1.0f, 10.0f, 0.0f);
+        GameObject instance = (GameObject) Instantiate(Resources.Load("Cube"),myVector,transform.rotation);
+    }
+}
